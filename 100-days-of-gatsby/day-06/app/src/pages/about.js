a0dda11ef0ba81a689fce2742062cd50c6febb4c
Rob Brennan <rob@therobbrennan.com>
2020-01-06 06:55:48 -0800

    Example use of a page query to retrieve siteMetadata title

--- 100-days-of-gatsby/day-06/app/src/pages/about.js
+++ 100-days-of-gatsby/day-06/app/src/pages/about.js
@@ -1,12 +1,24 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 
-export default () => (
+export default ({ data }) => (
   <Layout>
-    <h1>About Pandas Eating Lots</h1>
+    <h1>About {data.site.siteMetadata.title}</h1>
     <p>
       We're the only site running on your computer dedicated to showing the best
       photos and videos of pandas eating lots of food.
     </p>
   </Layout>
 )
+
+// Since this is a page, we can use a page query. By convention, it's located at the end of the Gatsby page file
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
