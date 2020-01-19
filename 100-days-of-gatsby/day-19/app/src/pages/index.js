9f1d658de408f766ab83f70eeb7c76850a653700
Rob Brennan <rob@therobbrennan.com>
2020-01-19 11:31:40 -0800

    Menu is now displaying dynamic content

--- 100-days-of-gatsby/day-19/app/src/pages/index.js
+++ 100-days-of-gatsby/day-19/app/src/pages/index.js
@@ -2,10 +2,12 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
+import Menu from '../components/menu'
 
 const IndexPage = () => (
   <Layout>
     <h1>Day 19</h1>
+    <Menu />
   </Layout>
 )
 
