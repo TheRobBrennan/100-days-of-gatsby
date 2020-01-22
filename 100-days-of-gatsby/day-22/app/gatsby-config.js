57106b6fc5f5072565df96637ecb2a3321abbddc
Rob Brennan <rob@therobbrennan.com>
2020-01-22 13:12:42 -0800

    Minor update to siteName

--- 100-days-of-gatsby/day-22/app/gatsby-config.js
+++ 100-days-of-gatsby/day-22/app/gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
-    siteName: `Gatsby Default Starter`,
+    siteName: `Gatsby Default Starter - Day 22`,
     exampleUrl: `https://github.com/gatsbyjs/gatsby/tree/master/examples/using-typescript`,
   },
   plugins: [
