710162e48ee9e509e04c63de800fc42e226e758b
Rob Brennan <rob@therobbrennan.com>
2020-01-23 11:24:32 -0800

    Added scaffolding for day 23

---
+++ 100-days-of-gatsby/day-23/README.md
@@ -0,0 +1,7 @@
+# Day 23 - Thursday, January 23rd, 2020
+
+After experiencing some surprising behavior with writing tests for the simple app I created yesterday, I wanted to really dive into what some of the gotchas were.
+
+Ultimately I'd love to incorporate this into a blog post at some point, but currently there are way too many other pressing priorities to give that draft an estimated timeline.
+
+## Scratchpad
---
+++ 100-days-of-gatsby/day-23/app/src/pages/index.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+
+import Index from './index'
+
+describe(`Index`, () => {
+  it(`should have tests written`, () => {
+    expect(true).toEqual(true)
+  })
+
+  it(`contains a greeting`, () => {
+    // Create mock data here. IndexPage will have the result of pageQuery available as this.props.data;
+    // we can simulate that GraphQL response here by passing in an explicit data prop
+    const mockPageQuery = {
+      site: {
+        siteMetadata: {
+          siteName: 'My site',
+          exampleUrl: 'https://myfakesite.com',
+        }
+      }
+    }
+    const { getByText, debug } = render(<Index data={mockPageQuery} />)
+    debug() // Display our rendered HTML
+
+    const greeting = getByText(/Hello/)
+    expect(greeting).toBeInTheDocument()
+
+  })
+})
